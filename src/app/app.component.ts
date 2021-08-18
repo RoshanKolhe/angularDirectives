40e4e83b119d9be608a248f41db7003320bd149e
Roshan <kolheroshan508@gmail.com>
2021-08-18 09:11:41 +0530

    simple app Using Angular Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularDirectives';
+  courses = [
+    {id:1,name:'course1'},
+    {id:2,name:'course2'},
+    {id:3,name:'course3'},
+  ];
+  viewMode = 'map';
+
+  addCourse(){
+    this.courses.push({id:4,name:'course4'});
+  }
 }
